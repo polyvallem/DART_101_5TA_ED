ca75013c331f081352b529c783bde4472598ea2a
Richar Cangui <richarsantiagocangui96@gmail.com>
2025-05-07 21:08:21 -0500

    add: create initial task for list manipulation in Dart

---
+++ TAREAS/2_listas.dart
@@ -0,0 +1,7 @@
+  // Tarea 2 
+  // 1. Crearse una lista de lo que sea 
+  // 2. Imprimir el último valor de la lista. Sin usar las posiciones quemadas
+  // listaZ[5]
+  // [1,2,3,4,5] -> 5
+  // [1,2,3,4,5,6,7] -> 7
+  // No usar el last 
