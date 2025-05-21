acafbb7c122d2d4564f01a7df0e9f4b742ac8c76
Richar Cangui <richarsantiagocangui96@gmail.com>
2025-05-20 19:32:05 -0500

    add: enhance Animal class hierarchy with Perro, Ave, and Pez subclasses and implement their respective methods

--- CURSO/LIVE/15_herencia.dart
+++ CURSO/LIVE/15_herencia.dart
@@ -1,16 +1,29 @@
 void main() {
+  final perro = Perro(nombre: "Pepe", tipo: "Perro", edad: 2);
+  final loro = Ave(nombre: "Cotorro", tipo: "Loro", edad: 1);
+  final delfin = Pez(nombre: "Fifi", tipo: "Delfin", edad: 1);
+  perro.comer();
+  loro.comer();
+  delfin.comer();
 
+  perro.caminar();
+  loro.volar();
+  delfin.nadar();
+
+  final perro2 = Raza(nombre: "Nombre", raza: "Chihuahua", edad: 1,tipo: "Perro");
+  print(perro2.razaAnimal);
+  perro2.comer();
+  perro2.caminar();
 }
 
 class Animal {
-  /// Atributos
+  /// Atributos -> Son inmutables -> porque sus valores no se pueden cambiar
   final String nombre;
   final String tipo;
   final int edad;
 
   /// Constructor
   Animal({required this.nombre, required this.tipo, required this.edad});
-
 
   /// Métodos
   void comer() {
@@ -20,17 +33,41 @@ class Animal {
   void respirar() {
     print("$nombre está respirando");
   }
+
+  String get ejemplo => "Hola";
+}
+
+class Perro extends Animal {
+  Perro({required super.nombre, required super.tipo, required super.edad});
+
+  void caminar() {
+    print("$nombre está caminando");
+  }
+}
+
+class Ave extends Animal {
+  Ave({required super.nombre, required super.tipo, required super.edad});
+
+ void volar() {
+    print("$nombre está volando");
+  }
   
 }
 
-class Perro {
-
+class Pez extends Animal {
+  Pez({required super.nombre, required super.tipo, required super.edad});
+  void nadar() {
+    print("$nombre está nadar");
+  }
 }
 
-class Ave {
 
-}
+class Raza extends Perro {
+  // Atributos
+  final String raza;
 
-class Pez {
+  Raza({required super.nombre, required super.tipo, required super.edad, required this.raza});
+
+  String get razaAnimal => "El perro $nombre es de raza $raza";
 
 }
